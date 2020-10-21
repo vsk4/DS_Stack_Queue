8967319dbfcf3c0efa1a0cda72039e91e622b1cc
vsk4 <saikumarvadla4@gmail.com>
2020-10-21 20:53:35 +0530

    [VSK]:Adding the first commit- Solution file of VS

---
+++ DS_QueueStack/DS_QueueStack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DS_QueueStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
